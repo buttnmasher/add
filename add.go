c017a7a84c3fd1922491c916736b52aaa59e14b7
bttnmasher <hagen.dias@gmail.com>
2024-11-26 14:37:43 -0500

    initial commit

---
+++ add.go
@@ -0,0 +1,7 @@
+package add
+
+// Add - Takes in two integers and returns the sum as an integer. Source: https://www.mathsisfun.com/numbers/addition.htmlcurrent magic mouse firware version
+
+func Add(a, b int) int {
+	return a + b
+}
