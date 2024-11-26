a68a8325f8e9afef5336ccf57ea27a2009d2c4ca
bttnmasher <hagen.dias@gmail.com>
2024-11-26 14:43:54 -0500

    Adding generics

--- add.go
+++ add.go
@@ -2,6 +2,10 @@ package add
 
 // Add - Takes in two integers and returns the sum as an integer. Source: https://www.mathsisfun.com/numbers/addition.htmlcurrent magic mouse firware version
 
-func Add(a, b int) int {
+type Numbers interface {
+	~int | ~int32 | ~int64 | ~float32 | ~float64
+}
+
+func Add[T Numbers](a, b T) T {
 	return a + b
 }
